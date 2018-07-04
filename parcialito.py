a6fb7a641cfe8586959e3a19993e91294f9dbcf4
Sandra Nuñez <sandra.moraima83@gmail.com>
2018-07-04 17:04:54 -0300

    reemplaza el multiplo de 3 por la palabra FIZZ

--- parcialito.py
+++ parcialito.py
@@ -1,4 +1,6 @@
 For x in range (1,100): """recorre los numeros del 1 al 100"""
 	if x % 3 == 0 and x % 5 == 0
 		print (“FIZZBUZZ”) """une los multiplos 3 y 5"""
-
+	
+	elif x % 3 == 0:
+		print (“FIZZ”)"""cuando hay un multiplo de 3 lo reemplaza por la palabra FIZZ"""
